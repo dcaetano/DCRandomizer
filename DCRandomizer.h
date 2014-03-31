6320e1cf8b77da296c06cbfa93744e80edfbed18
Danilo Caetano <dcaetano@itriagehealth.com>
2014-03-31 11:58:58 -0600

    First commit

---
+++ DCRandomizer.h
@@ -0,0 +1,16 @@
+//
+//  DCRandomizer.h
+//  DC1
+//
+//  Created by A662564 on 3/31/14.
+//  Copyright (c) 2014 A662564. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCRandomizer : NSObject
+
++(NSMutableArray*)randomizeArray:(NSMutableArray*)arrayIn;
+
+@end
+
